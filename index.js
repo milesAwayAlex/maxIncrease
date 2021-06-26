f6d7aceedd59ec69c361a6145346865ca5e043b1
Alex M <milesAwayAlex@gmail.com>
2021-06-26 13:59:19 -0700

    Add comments

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const increase = arr => {
+  // simple algorithm in O(n), starting from the right
   const res = arr.reduceRight(
     (a, e) => {
+      // check if the point is highest so far (to the right)
       if (e > a.maxRight) a.maxRight = e;
+      // find increase, set -1 for no increase
       const d = a.maxRight - e || -1;
+      // check if the increase is largest so far
       if (d > a.incRight) a.incRight = d;
       return a;
     },
+    // initialize the accumulator
     {incRight: -1, maxRight: -Infinity}
   );
   return res.incRight;
